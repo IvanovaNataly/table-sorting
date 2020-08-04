05d7f40499024f44078061ec17514fc57ef3f2a6
IvanovaNataly <letter4natalia@gmail.com>
2020-08-04 17:44:03 +0300

    init

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {HTTPService} from "../../services/http-service.service";
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+  public serverError: boolean;
+  public tableData: Array<any>;
+
+  constructor( private httpService: HTTPService ) { }
+
+  ngOnInit() {
+    this.httpService.getData().subscribe( res => {
+      if (res) {
+        this.tableData = res;
+      }
+      else {
+        this.serverError = true;
+      }
+    });
+  }
+
+}
---
+++ src/app/services/http-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const routes = {
+  templateUrl: () => `assets/data/data.json`,
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HTTPService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getData(): Observable<any> {
+    console.log(this.http.get(routes.templateUrl()));
+    return this.http.get(routes.templateUrl());
+  }
+}
