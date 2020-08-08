f5a433b405537ff525107f05d3899d26aee35339
IvanovaNataly <letter4natalia@gmail.com>
2020-08-08 12:45:47 +0300

    sorting added

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HTTPService} from "../../services/http-service.service";
+import { ProductProperties } from '../../enums/productProp';
 
 @Component({
   selector: 'app-table',
@@ -9,6 +10,7 @@ import {HTTPService} from "../../services/http-service.service";
 export class TableComponent implements OnInit {
   public serverError: boolean;
   public tableData: Array<any>;
+  public sortValue: ProductProperties;
 
   constructor( private httpService: HTTPService ) { }
 
@@ -18,6 +20,34 @@ export class TableComponent implements OnInit {
     }, (error) => {
       this.serverError = true;
     });
+  }
+
+  sortTable(ascending: boolean, propKey: ProductProperties) {
+    this.sortValue = propKey;
+    if (propKey === ProductProperties.ProductId ||
+      propKey === ProductProperties.CatId ||
+      propKey === ProductProperties.ProductSize) {
+      this.tableData.sort( this.compareNumeric.bind(this) );
+    } else {
+      this.tableData.sort( this.compareString.bind(this) );
+    }
+    if (!ascending) {
+      this.tableData.reverse();
+    }
+  }
+
+  compareString( a, b ) {
+    if ( a[this.sortValue] < b[this.sortValue] ){
+      return -1;
+    }
+    if ( a[this.sortValue] > b[this.sortValue] ){
+      return 1;
+    }
+    return 0;
+  }
+
+  compareNumeric( a, b ) {
+    return a[this.sortValue] - b[this.sortValue];
   }
 
 }
---
+++ src/app/enums/productProp.ts
@@ -0,0 +1,9 @@
+export enum ProductProperties {
+  ProductSize = 'productSize',
+  Item = 'item',
+  Plu = 'plu_upc',
+  Price = 'price',
+  ProductId = 'productId',
+  CatId = 'catId',
+  Uom = 'uom'
+}
